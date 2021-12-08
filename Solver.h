8284ddfc125db4860e38e790cd86c61bcbd9af2e
Alex <15199219+muskit@users.noreply.github.com>
2021-12-08 00:00:31 -0800

    start of user input, fundamental changes

--- Solver.h
+++ Solver.h
@@ -1,9 +1,15 @@
 #ifndef Solver_h
 #define Solver_h
 
+#include <string>
+
+// All Solver classes should be subclasses of this.
+
 class Solver {
 public:
-    virtual void solve();
+    // Returns a string that tells the result.
+    // This string will be used in final output.
+    virtual std::string solve();
 };
 
 #endif // Solver_h
